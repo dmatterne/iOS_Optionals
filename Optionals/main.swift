6e3d23891534514fd7542f863fed1a1248692266
David Matterne <david.matterne@gmail.com>
2016-09-26 14:50:40 +0200

    Final solution commit

--- Optionals/main.swift
+++ Optionals/main.swift
@@ -10,3 +10,46 @@ import Foundation
 
 print("Hello, World!")
 
+let possibleNumber = "123"
+let actualNumber = Int(possibleNumber)
+
+print(actualNumber)
+
+let possibleNumber2 = "ABC"
+let actualNumber2 = Int(possibleNumber2)
+
+print(actualNumber2)
+
+print(actualNumber!)
+
+if actualNumber != nil {
+    
+    print("Conversion succesfull")
+    
+} else {
+    
+    print("Conversion unsuccessfull")
+}
+
+
+if actualNumber2 != nil {
+
+    print("Conversion succesfull")
+    
+} else {
+
+    print("Conversion unsuccessfull")
+}
+
+if let anotherActualNumber = Int(possibleNumber) {
+
+    print("OPtional Binding succesfull for \(anotherActualNumber)")
+
+}
+
+var name: String? = “Richard”
+if name != nil {
+    print("Name is \(name!)")
+} else {
+    print("Name is nil")
+}
